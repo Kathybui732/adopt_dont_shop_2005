5984e24ba022c4a9c9e66d28dc3d25911d1a2f4b
Kathy Bui <kathy.bui1992@gmail.com>
2020-07-03 10:49:38 -0600

    Create pet index feature

---
+++ app/models/pet.rb
@@ -0,0 +1,7 @@
+class Pet < ApplicationRecord
+  validates_presence_of :image
+  validates_presence_of :name
+  validates_presence_of :approximate_age
+  validates_presence_of :sex
+  validates_presence_of :shelter
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200702190531) do
+ActiveRecord::Schema.define(version: 20200702202822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20200702190531) do
   create_table "pets", force: :cascade do |t|
     t.string "image"
     t.string "name"
-    t.string "approximate_age"
+    t.integer "approximate_age"
     t.string "sex"
     t.string "shelter"
     t.datetime "created_at", null: false
